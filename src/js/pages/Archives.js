95fbc3dcaf9c995775829369bd6f4edb79f6f7d7
Hideto Yasunori <hideto0118yasunori@gmail.com>
2019-12-23 17:25:30 -0800

    Mostly done

--- src/js/pages/Archives.js
+++ src/js/pages/Archives.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export default class Settings extends React.Component {
   render() {
+    // const query = new URLSearchParamas(tnis.props.location.search)
+    // let message
+    //     = (this.props.match.params.article ? this.props.match.params.article + ", " : "")
+    //     + "date=" + query.get("date") + ", filter=" + query.get("filter");
+
     return (
-      <h1>Archives</h1>
+      <h1>Archives ({this.props.match.params.article})</h1>
     )
   }
 }
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, Link, withRouter } from "react-router-dom";
 
-export default class Layout extends React.Component {
+class Layout extends React.Component {
+  navigate() {
+    console.log(this.props.history);
+    this.props.history.push("/");
+    // this.props.history.replace("/");
+  }
+
   render () {
     return (
       <div>
         <h1>KillerNews.net</h1>
         {this.props.children}
-        <Link to="archives"><button class="btn btn-danger">archives</button></Link> 
-        <Link to="settings"><button class="btn btn-success">settings</button></Link>
+        <Link to="/archives/some-other-articles?date=yesterday&fileter=none" class="btn btn-warning">archives (article name shows up)</Link>
+        <Link to="/archives?date=today&filter=hot" class="btn btn-danger">archives</Link> 
+        <NavLink to="/settings/main" class="btn btn-success" activeClassName="btn-danger">settings</NavLink>
+        <Link to="/settings/extra" class="btn btn-success">settings (extra)</Link>
+        <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
       </div>
     );
   }
 }
+
+export default withRouter(Layout);
